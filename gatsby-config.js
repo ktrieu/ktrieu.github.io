13d29d1e5bf9f8e8391f6f620b50f645ef9d8436
Kevin Trieu <kevin.trieu@setter.com>
2020-10-04 00:20:44 -0400

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `kevintrieu.com`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `The home of all Kevin Trieu-related content on the web.`,
+    author: `Kevin Trieu`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
