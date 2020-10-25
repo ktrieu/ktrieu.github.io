ee90c03dae9424e0b8a2c5adc9fc3f26b5958baa
Kevin Trieu <kevin.trieu@setter.com>
2020-10-24 22:18:35 -0400

    Add an autosuggest box for notable figures

--- src/pages/widgets/gamer.tsx
+++ src/pages/widgets/gamer.tsx
@@ -1,23 +1,101 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
+import Layout from "../../components/layout"
 import {
   NotableFigure,
   searchFigures,
 } from "../../components/widgets/gamer/api"
+import Autosuggest, {
+  InputProps,
+  RenderInputComponent,
+  RenderSuggestion,
+  RenderSuggestionsContainer,
+  SuggestionsFetchRequestedParams,
+} from "react-autosuggest"
 
+const SuggestionContainer = styled.div`
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+  }
+`
+
+const renderSuggestionContainer: RenderSuggestionsContainer = params => {
+  return (
+    <SuggestionContainer {...params.containerProps}>
+      {params.children}
+    </SuggestionContainer>
+  )
+}
+
+const SuggestionDiv = styled.div`
+  font-size: 12px;
+  line-height: 1;
+  border: 1px solid black;
+  padding: 10px;
+`
+
+const renderSuggestion: RenderSuggestion<NotableFigure> = (suggestion, _) => {
+  return (
+    <SuggestionDiv>
+      <p>{suggestion.name}</p>
+      <p>
+        <small>{suggestion.description}</small>
+      </p>
+    </SuggestionDiv>
+  )
+}
+
+// The inputProps override the styled components one, so we have to be a little clever
+const StyledInput = styled.input`
+  width: 100%;
+`
+
+const renderInput: RenderInputComponent<NotableFigure> = inputProps => {
+  // types don't quite match here but whatever
+  return <StyledInput {...(inputProps as any)} />
+}
+
+const GamerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 const GamerWidget = () => {
-  const [data, setData] = useState<NotableFigure[] | null>(null)
+  const [suggestValue, setSuggestValue] = useState<string>("")
+  const [suggestions, setSuggestions] = useState<NotableFigure[]>([])
 
-  useEffect(() => {
-    searchFigures("John Cena")
-      .then(figures => {
-        setData(figures)
-      })
-      .catch(err => {
-        alert(err.message)
-      })
-  }, [])
+  const fetchSuggestions = (params: SuggestionsFetchRequestedParams) => {
+    searchFigures(params.value).then(figures => {
+      setSuggestions(figures)
+    })
+  }
 
-  return data ? <pre>{JSON.stringify(data, null, 2)}</pre> : null
+  const suggestProps: InputProps<NotableFigure> = {
+    placeholder: "Enter a name...",
+    value: suggestValue,
+    onChange: (_, params) => {
+      setSuggestValue(params.newValue)
+    },
+  }
+
+  return (
+    <Layout>
+      <GamerContainer>
+        <h1>Were They A Gamer?</h1>
+      </GamerContainer>
+      <Autosuggest
+        suggestions={suggestions}
+        getSuggestionValue={(suggestion: NotableFigure) => suggestion.name}
+        onSuggestionsFetchRequested={fetchSuggestions}
+        onSuggestionsClearRequested={() => setSuggestions([])}
+        renderSuggestionsContainer={renderSuggestionContainer}
+        renderSuggestion={renderSuggestion}
+        renderInputComponent={renderInput}
+        inputProps={suggestProps}
+      />
+    </Layout>
+  )
 }
 
 export default GamerWidget
